3aae0154f42eebe6929bbc2c575e7db885053289
svprdga <svprdga@reprom.io>
2021-07-12 18:20:50 +0200

    Add more examples

--- example/lib/result_screen.dart
+++ example/lib/result_screen.dart
@@ -17,6 +17,42 @@ class ResultScreen extends StatelessWidget {
     String title;
     String body;
     switch (barcode.valueType) {
+      case BarcodeValueType.CONTACT_INFO:
+        title = 'Contact data';
+        body = _getContactInfoBody();
+        break;
+      case BarcodeValueType.EMAIL:
+        title = 'Email';
+        body = _getEmailBody();
+        break;
+      case BarcodeValueType.ISBN:
+        title = 'ISBN code';
+        body = _getIsbnBody();
+        break;
+      case BarcodeValueType.PHONE:
+        title = 'Phone';
+        body = _getPhoneBody();
+        break;
+      case BarcodeValueType.PRODUCT:
+        title = 'Product';
+        body = _getProductBody();
+        break;
+      case BarcodeValueType.SMS:
+        title = 'SMS';
+        body = _getSmsBody();
+        break;
+      case BarcodeValueType.TEXT:
+        title = 'Plain text';
+        body = _getTextBody();
+        break;
+      case BarcodeValueType.URL:
+        title = 'Web address';
+        body = _getUrlBody();
+        break;
+      case BarcodeValueType.WIFI:
+        title = 'WiFi';
+        body = _getWifiBody();
+        break;
       case BarcodeValueType.LOCATION:
         title = 'Location';
         body = _getLocationBody();
@@ -24,6 +60,10 @@ class ResultScreen extends StatelessWidget {
       case BarcodeValueType.CALENDAR_EVENT:
         title = 'Calendar event';
         body = _getCalendarBody();
+        break;
+      case BarcodeValueType.DRIVER_LICENSE:
+        title = 'Driver license';
+        body = _getDriverLicenseBody();
         break;
       default:
         title = 'Unknown';
@@ -72,6 +112,123 @@ class ResultScreen extends StatelessWidget {
         - Summary: ${barcodeCalendar.summary}
         - Status: ${barcodeCalendar.status}
         - Organizer: ${barcodeCalendar.organizer} 
+    ''';
+  }
+
+  String _getContactInfoBody() {
+    final barcodeContactInfo = barcode as BarcodeContactInfo;
+
+    return '''This code contains contact information:
+    
+        - Addresses: ${barcodeContactInfo.addresses}
+        - Emails: ${barcodeContactInfo.emails}
+        - Name: ${barcodeContactInfo.emails}
+        - Phones: ${barcodeContactInfo.phones}
+        - Title: ${barcodeContactInfo.title}
+        - Urls: ${barcodeContactInfo.urls}
+        - Organization: ${barcodeContactInfo.organization}
+    ''';
+  }
+
+  String _getDriverLicenseBody() {
+    final barcodeDriverLicense = barcode as BarcodeDriverLicense;
+
+    return '''This code contains a driver license:
+    
+        - City: ${barcodeDriverLicense.addressCity}
+        - State: ${barcodeDriverLicense.addressState}
+        - Street: ${barcodeDriverLicense.addressStreet}
+        - Zip: ${barcodeDriverLicense.addressZip}
+        - BirthDate: ${barcodeDriverLicense.birthDate != null ? barcodeDriverLicense.birthDate!.toIso8601String() : '-'}
+        - Document type: ${barcodeDriverLicense.documentType}
+        - Expiry date: ${barcodeDriverLicense.expiryDate != null ? barcodeDriverLicense.expiryDate!.toIso8601String() : '-'}
+        - First name: ${barcodeDriverLicense.firstName}
+        - Gender: ${barcodeDriverLicense.gender}
+        - Issue date: ${barcodeDriverLicense.issueDate != null ? barcodeDriverLicense.issueDate!.toIso8601String() : '-'}
+        - Issuing country: ${barcodeDriverLicense.issuingCountry}
+        - Last name: ${barcodeDriverLicense.lastName}
+        - Middle name: ${barcodeDriverLicense.middleName}
+        - License number: ${barcodeDriverLicense.licenseNumber}
+    ''';
+  }
+
+  String _getEmailBody() {
+    final barcodeEmail = barcode as BarcodeEmail;
+
+    return '''This code contains an email:
+    
+        - Address: ${barcodeEmail.address}
+        - Body: ${barcodeEmail.body}
+        - Subject: ${barcodeEmail.subject}
+        - Type: ${barcodeEmail.emailType.toString()}
+    ''';
+  }
+
+  String _getPhoneBody() {
+    final barcodePhone = barcode as BarcodePhone;
+
+    return '''This code contains a phone:
+    
+        - Number: ${barcodePhone.number}
+        - Type: ${barcodePhone.phoneType.toString()}
+    ''';
+  }
+
+  String _getSmsBody() {
+    final barcodeSms = barcode as BarcodeSms;
+
+    return '''This code contains an SMS:
+    
+        - Message: ${barcodeSms.message}
+        - Phone number: ${barcodeSms.phoneNumber}
+    ''';
+  }
+
+  String _getUrlBody() {
+    final barcodeUrl = barcode as BarcodeUrl;
+
+    return '''This code contains an URL:
+    
+        - Title: ${barcodeUrl.title}
+        - Url: ${barcodeUrl.url}
+    ''';
+  }
+
+  String _getWifiBody() {
+    final barcodeWifi = barcode as BarcodeWifi;
+
+    return '''This code contains WiFi data:
+    
+        - SSID: ${barcodeWifi.ssid}
+        - Password: ${barcodeWifi.password}
+        - Encryption type: ${barcodeWifi.encryptionType}
+    ''';
+  }
+
+  String _getIsbnBody() {
+    final barcodeIsbn = barcode as BarcodeIsbn;
+
+    return '''This code contains a ISBN code:
+    
+        - Raw value: ${barcodeIsbn.rawValue}
+    ''';
+  }
+
+  String _getProductBody() {
+    final barcodeProduct = barcode as BarcodeProduct;
+
+    return '''This code contains a product code:
+    
+        - Raw value: ${barcodeProduct.rawValue}
+    ''';
+  }
+
+  String _getTextBody() {
+    final barcodeText = barcode as BarcodeText;
+
+    return '''This code contains plain text:
+    
+        - Raw value: ${barcodeText.rawValue}
     ''';
   }
 }
